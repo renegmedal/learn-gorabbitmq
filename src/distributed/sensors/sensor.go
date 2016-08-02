ab27ff843b2ea36f0affa2d8925715fac060191a
rene <renegmed@yahoo.com>
2016-08-02 06:36:55 -0400

    2: config param from command line.

---
+++ src/distributed/sensors/sensor.go
@@ -0,0 +1,13 @@
+package main
+
+import "flag"
+
+var name = flag.String("name", "sensor", "name of the sensor")
+var freq = flag.Uint("freq", 5, "update frequency in cycles/sec")
+var max = flag.Float64("max", 5., "maximum value for generated readings")
+var min = flag.Float64("min", 1., "minimum value for generated readings")
+var stepSize = flag.Float64("step", 0.1, "maximum allowable change per measurement")
+
+func main() {
+	flag.Parse()
+}
